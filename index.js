d4524a2e3fa3920db87bba942a8369bdae42a801
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-10-26 12:03:06 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop("Bob");
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("Milo")
+}
+function appendCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.push("Broom");
+    return copyOfCats;
+}
+function prependCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.unshift("Arnold");
+    return copyOfCats;
+}
+function removeLastCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.pop("Garfield");
+    return copyOfCats;
+}
+function removeFirstCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.shift("Milo");
+    return copyOfCats;
+}
